6bf6d1659a7fca720e97ba2a1617c8b3a0087b3f
Reid Paape <wrpaape@gmail.com>
2015-10-06 12:55:37 -0500

    generated models and main controller

---
+++ app/assets/javascripts/components/index.js.jsx
@@ -0,0 +1,6 @@
+var Index = React.createClass({
+
+  render: function() {
+    return <div />;
+  }
+});
---
+++ db/migrate/20151006174805_create_perks.rb
@@ -0,0 +1,10 @@
+class CreatePerks < ActiveRecord::Migration
+  def change
+    create_table :perks do |t|
+      t.string :name
+      t.text :details
+
+      t.timestamps null: false
+    end
+  end
+end
