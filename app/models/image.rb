a53da8618cd549d32fc4d0056bd28454f26b97a0
Reid Paape <wrpaape@gmail.com>
2015-10-07 19:21:00 -0500

    headshot.jpg added to members on creation

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,2 +1,9 @@
 class Image < Asset
+  before_create :set_filename
+
+  private
+
+  def set_filename
+    filename.prepend("#{parent.name.sub(" ", "_")}-") 
+  end
 end
--- app/models/member.rb
+++ app/models/member.rb
@@ -1,4 +1,11 @@
 class Member < ActiveRecord::Base
+  after_create :add_headshot
   has_many :contacts
   has_many :images, as: :parent
+
+  private
+
+  def add_headshot
+    images.create(filename: "headshot.jpg")
+  end
 end
