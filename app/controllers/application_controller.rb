e4e7dd1de4a070832f4191fb551f6cbefac3cdb6
Reid Paape <wrpaape@gmail.com>
2015-10-14 21:01:34 -0500

    goin in DRY

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,11 +5,11 @@ class ApplicationController < ActionController::Base
 
   protected
 
-  def results_proc(model, search_for)
+  def self.results_proc(model, search_for)
     pluck_results =
       case search_for
         when :name
-          model == Member ? [:path_show, :name] : [:path_show, :name, :title] 
+          model == Member ? [:path_show, :name, :title] : [:path_show, :name] 
         when :category
           return Proc.new { |input| %w(products modifications).grep(Regexp.new(input, "i")).map { |cat| [eval("#{cat}_path"), cat] } }
         when :info
@@ -22,7 +22,7 @@ class ApplicationController < ActionController::Base
     Proc.new { |input| model.where("#{search_for} ~* ?", input).pluck(*pluck_results) }
   end
 
-  def display_proc(model, search_for)
+  def self.display_proc(model, search_for)
     return Proc.new { |result| "#{result[1]} (#{result[2]})" } if model == Member
     case search_for
       when :info
@@ -32,71 +32,25 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  PRODUCTS = {
-    products: {
-      name: {
-        results: Proc.new { |input| Product.where("name ~* ?", input).pluck(:path_show, :name) },
-        display: Proc.new { |result| result[1] }
-      },
-      category: {
-        results: Proc.new { |input| %w(products modifications).grep(Regexp.new(input, "i")).map { |cat| [eval("#{cat}_path"), cat] } },
-        display: Proc.new { |result| result[1] }
-      },
-      info: {
-        results: Proc.new { |input| Product.where("info ~* ?", input).pluck(:path_show, :info, :name) },
-        display: Proc.new { |result, input| "#{result[1][Regexp.new("\\w*\\.*\\s*\\w*#{input}\\w*\\.*\\s*\\w*", "i")]}▓(#{result[2]})" }
-      }
-    }
-  }
-  GOODS = {
-    goods: {
-      name: {
-        results: Proc.new { |input| Good.where("name ~* ?", input).pluck(:path_show, :name) },
-        display: Proc.new { |result| result[1] }
-      },
-      info: {
-        results: Proc.new { |input| Good.where("info ~* ?", input).pluck(:path_show, :info, :name) },
-        display: Proc.new { |result, input| "#{result[1][Regexp.new("\\w*\\.*\\s*\\w*#{input}\\w*\\.*\\s*\\w*", "i")]}▓(#{result[2]})" }
-      }
-    }
-  }
-  MODS = {
-    goods: {
-      name: {
-        results: Proc.new { |input| Mod.where("name ~* ?", input).pluck(:path_show, :name) },
-        display: Proc.new { |result| result[1] }
-      },
-      info: {
-        results: Proc.new { |input| Mod.where("info ~* ?", input).pluck(:path_show, :info, :name) },
-        display: Proc.new { |result, input| "#{result[1][Regexp.new("\\w*\\.*\\s*\\w*#{input}\\w*\\.*\\s*\\w*", "i")]}▓(#{result[2]})" }
-      }
-    }
-  }
-  MEMBERS = {
-      members: {
-      name: {
-        results: Proc.new { |input| Member.where("name ~* ?", input).pluck(:path_show, :name, :title) },
-        display: Proc.new { |result| "#{result[1]} (#{result[2]})" }
-      },
-      title: {
-        results: Proc.new { |input| Member.where("title ~* ?", input).pluck(:path_show, :title, :name) },
-        display: Proc.new { |result| "#{result[1]} (#{result[2]})" }
-      }
-    }
-  }
-  PDFS = {
-    pdfs: {
-      filename: {
-        results: Proc.new { |input| Pdf.where("filename ~* ?", input).pluck(:path_alt, :filename) },
-        display: Proc.new { |result| result[1] }
-      }
-    }
-  }
-  ALL = {
-    products: PRODUCTS[:products],
-    members: MEMBERS[:members],
-    pdfs: PDFS[:pdfs]
-  }
+  def self.build_procs(model, search_for)
+    Hash[[:results, :display].map { |proc_type| [proc_type, eval("#{proc_type}_proc(model, search_for)")] }]
+  end
+
+  def self.build_categories(model, search_fors)
+    Hash[search_fors.map { |search_for| [search_for, build_procs(model, search_for)] }]
+  end
+
+  def self.build_constant(model, search_fors)
+    Hash[model.to_s.underscore.pluralize.to_sym, build_categories(model, search_fors)]
+  end
+
+  PRODUCTS = build_constant(Product, [:name, :category, :info])
+  GOODS = build_constant(Good, [:name, :info])
+  MODS = build_constant(Mod, [:name, :info])
+  MEMBERS = build_constant(Member, [:name, :title])
+  PDFS = build_constant(Pdf, [:filename])
+  
+  ALL = [PRODUCTS, MEMBERS, PDFS].reduce({}, :merge)
 
   def query(pool = ALL, input)
     output = []
