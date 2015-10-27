5a156119600e4d969f108e4ed8af648584740a86
Reid Paape <wrpaape@gmail.com>
2015-10-26 21:41:30 -0500

    one last deploy

--- app/assets/javascripts/components/nav_bar/contact_content.js.jsx
+++ app/assets/javascripts/components/nav_bar/contact_content.js.jsx
@@ -3,7 +3,7 @@
 
 var ContactContent = React.createClass({
   render: function() {
-    var contactLis, display, className, contacts, image, name;
+    var contactLis, display, className, contacts, image, link, name;
     var allContacts = this.props.contactables.map(function(contactable) {
       contactLis = contactable.contacts.map(function(contact) {
         display = contact.info_display.map(function(line, i) {
@@ -26,10 +26,14 @@ var ContactContent = React.createClass({
 
       image = React.createElement(window.ImageLink, contactable.image);
 
-      name = React.createElement('a', {
+      link = React.createElement('a', {
         className: contactable.type_display,
         href: contactable.path_show
       }, contactable.name_display);
+
+      name = React.createElement('div', {
+        className: 'name'
+      }, link);
 
       return React.createElement('div', {
         key: contactable.key,
